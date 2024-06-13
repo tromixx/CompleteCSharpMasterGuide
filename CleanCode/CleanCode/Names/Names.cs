9e8d0e14caaf6f77cb92e1eb65d339e9442461e2
Tomas Venere <tomasvenere@Tomass-Laptop.local>
2024-06-13 19:44:55 -0400

    Adding Clean code

---
+++ CleanCode/CleanCode/Names/Names.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+
+namespace CleanCode.Names
+{
+    public class Names
+    {
+        public Bitmap GenerateImage(string path)
+        {
+            var bitMap = new Bitmap(path);
+            var graphics = Graphics.FromImage(b);
+            graphics.DrawString("a", SystemFonts.DefaultFont, SystemBrushes.Desktop, new PointF(0, 0));
+            graphics.DrawString("b", SystemFonts.DefaultFont, SystemBrushes.Desktop, new PointF(0, 20));
+            graphics.DrawString("c", SystemFonts.DefaultFont, SystemBrushes.Desktop, new PointF(0, 30));
+            return bitMap;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # CompleteCSharpMasterGuide
-Howdy! Here is a Complete CSharp Master Guide for those who want to take your C# knowledge to the next level.
+Howdy howdy!
+Here is a complete guide of all the topics you need to be a great developer.
+
+Coming up soon I will add a section for Solid Principles,
