5ba843c8e52b7021b9615f013d944fa9c5ba830c
Tomas Venere <tomasvenere@Tomass-Laptop.local>
2024-06-16 15:26:13 -0400

    Adding minor things

--- CleanCode/CleanCode/FullRefactoring/Example1.cs
+++ CleanCode/CleanCode/FullRefactoring/Example1.cs
@@ -18,49 +18,52 @@ namespace Project.UserControls
 
             if (Page.IsPostBack)
             {
-                PostValidator validator = new PostValidator();
-                Post entity = new Post()
-                {
-                    // Map form fields to entity properties
-                    Id = Convert.ToInt32(PostId.Value),
-                    Title = PostTitle.Text.Trim(),
-                    Body = PostBody.Text.Trim()
-                };
-                ValidationResult results = validator.Validate(entity);
-
-                if (results.IsValid)
-                {
-                    // Save to the database and continue to the next page
-                    DBContext.Posts.Add(entity);
-                    DBContext.SaveChanges();
-                }
-                else
-                {
-                    BulletedList summary = (BulletedList)FindControl("ErrorSummary");
-
-                    // Display errors to the user
-                    foreach (var failure in results.Errors)
-                    {
-                        Label errorMessage = FindControl(failure.PropertyName + "Error") as Label;
-
-                        if (errorMessage == null)
-                        {
-                            summary.Items.Add(new ListItem(failure.ErrorMessage));
-                        }
-                        else
-                        {
-                            errorMessage.Text = failure.ErrorMessage;
-                        }
-                    }
-                }
+                TrySavePost();
             }
             else
             {
-                // Display form
                 Post entity = DBContext.Posts.SingleOrDefault(p => p.Id == Convert.ToInt32(Request.QueryString["id"]));
                 PostBody.Text = entity.Body;
                 PostTitle.Text = entity.Title;
+            }
+        }
 
+        private void TrySavePost()
+        {
+            PostValidator validator = new PostValidator();
+            Post entity = new Post()
+            {
+                // Map form fields to entity properties
+                Id = Convert.ToInt32(PostId.Value),
+                Title = PostTitle.Text.Trim(),
+                Body = PostBody.Text.Trim()
+            };
+            ValidationResult results = validator.Validate(entity);
+
+            if (results.IsValid)
+            {
+                // Save to the database and continue to the next page
+                DBContext.Posts.Add(entity);
+                DBContext.SaveChanges();
+            }
+            else
+            {
+                BulletedList summary = (BulletedList)FindControl("ErrorSummary");
+
+                // Display errors to the user
+                foreach (var failure in results.Errors)
+                {
+                    Label errorMessage = FindControl(failure.PropertyName + "Error") as Label;
+
+                    if (errorMessage == null)
+                    {
+                        summary.Items.Add(new ListItem(failure.ErrorMessage));
+                    }
+                    else
+                    {
+                        errorMessage.Text = failure.ErrorMessage;
+                    }
+                }
             }
         }
 
